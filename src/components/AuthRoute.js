f01c9603ea19632184e47e8af96cb761cc42f6ec
lionel-imac <flf241892@sina.cn>
2024-04-28 22:40:52 +0800

    AuthRoute

---
+++ src/components/AuthRoute.js
@@ -0,0 +1,14 @@
+import { getToken } from "@/utils/token"
+import { Navigate } from "react-router-dom"
+
+//高阶组件
+const AuthRoute = ({ children }) => {
+    const token = getToken()
+    if (token) {
+        return <>children</>
+    } else {
+        return <Navigate to='/login' replace />
+    }
+}
+
+export default AuthRoute
