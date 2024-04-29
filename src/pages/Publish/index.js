9fac6e568fee6a267bd8f6ebde34cfc928289539
lionel-imac <flf241892@sina.cn>
2024-04-29 22:33:57 +0800

    upload file

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -1,5 +1,129 @@
+import {
+    Card,
+    Breadcrumb,
+    Form,
+    Button,
+    Radio,
+    Input,
+    Upload,
+    Space,
+    Select,
+    message
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { useEffect, useState } from 'react'
+import { getChannelAPI, publishAPI } from '@/apis/article'
+
+const { Option } = Select
+
 const Publish = () => {
-    return <div>this is publish</div>
+
+    const [channelList, setChannelList] = useState([])
+    useEffect(() => {
+        const getChannelList = async () => {
+            const res = await getChannelAPI()
+            setChannelList(res.data.channels)
+        }
+        getChannelList()
+    }, [])
+
+    const onFinish = async (formValue) => {
+        console.log(formValue);
+        const { channel_id, content, title } = formValue
+        const params = {
+            title,
+            content,
+            cover: {
+                type: 0,
+                images: []
+            },
+            channel_id,
+        }
+        await publishAPI(params)
+        message.success('Publish Success')
+    }
+
+    const [imageList, setImageList] = useState([])
+    const onUploadChange = (info) => {
+        setImageList(info.fileList)
+    }
+
+    return (
+        <div className="publish">
+            <Card
+                title={
+                    <Breadcrumb items={[
+                        { title: <Link to={'/'}>首页</Link> },
+                        { title: '发布文章' },
+                    ]}
+                    />
+                }
+            >
+                <Form
+                    labelCol={{ span: 4 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ type: 1 }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        label="标题"
+                        name="title"
+                        rules={[{ required: true, message: '请输入文章标题' }]}
+                    >
+                        <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+                    </Form.Item>
+                    <Form.Item
+                        label="频道"
+                        name="channel_id"
+                        rules={[{ required: true, message: '请选择文章频道' }]}
+                    >
+                        <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+                            {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="封面">
+                        <Form.Item name="type">
+                            <Radio.Group>
+                                <Radio value={1}>单图</Radio>
+                                <Radio value={3}>三图</Radio>
+                                <Radio value={0}>无图</Radio>
+                            </Radio.Group>
+                        </Form.Item>
+                        <Upload
+                            listType="picture-card"
+                            name='image'
+                            action={'http://geek.itheima.net/v1_0/upload'}
+                            onChange={onUploadChange}
+                            showUploadList
+                        >
+                            <div style={{ marginTop: 8 }}>
+                                <PlusOutlined />
+                            </div>
+                        </Upload>
+                    </Form.Item>
+                    <Form.Item
+                        label="内容"
+                        name="content"
+                        rules={[{ required: true, message: '请输入文章内容' }]}
+                    >
+                        <ReactQuill className='publish-quill' theme='snow' placeholder='请输入文章内容' />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 4 }}>
+                        <Space>
+                            <Button size="large" type="primary" htmlType="submit">
+                                发布文章
+                            </Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 
 export default Publish
